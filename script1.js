b9417c7d1cc06be37ddbd908f7c103eeac47f828
harika-2001 <65670592+harika-2001@users.noreply.github.com>
2021-06-13 03:07:57 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,61 @@
+function show1() {
+    document.getElementById('tel').style.display = 'inline';
+}
+function hide1() {
+    document.getElementById('tel').style.display = 'none';
+}
+function show2() {
+    document.getElementById('fce').style.display = 'inline';
+}
+function hide2() {
+    document.getElementById('fce').style.display = 'none';
+}
+function show3() {
+    document.getElementById('twi').style.display = 'inline';
+}
+function hide3() {
+    document.getElementById('twi').style.display = 'none';
+}
+function show4() {
+    document.getElementById('mil').style.display = 'inline';
+}
+function hide4() {
+    document.getElementById('mil').style.display = 'none';
+}
+function show5() {
+    document.getElementById('git').style.display = 'inline';
+}
+function hide5() {
+    document.getElementById('git').style.display = 'none';
+}
+function show6() {
+    document.getElementById('bit').style.display = 'inline';
+}
+function hide6() {
+    document.getElementById('bit').style.display = 'none';
+}
+function func1(){
+  document.getElementById('one').style.display = 'block';
+  document.getElementByID('two').style.display = 'none';
+  document.getElementByID('three').style.display = 'none';
+}
+function func2(){
+  document.getElementById('one').style.display = 'none';
+  document.getElementByID('two').style.display = 'block';
+  document.getElementByID('three').style.display = 'none';
+}
+function func3(){
+  document.getElementById('one').style.display = 'none';
+  document.getElementByID('two').style.display = 'none';
+  document.getElementByID('three').style.display = 'block';
+}
+function collap1(){
+  document.getElementById('minus1').style.display='inline';
+  document.getElementById('plus1').style.display = 'none';
+  document.getElementById('hid1').style.display  = 'block;'
+}
+function constr1(){
+  document.getElementById('minus1').style.display='none';
+  document.getElementById('plus1').style.display = 'inline';
+  document.getElementById('hid1').style.display  = 'none;'
+}
---
+++ script2.js
@@ -0,0 +1,40 @@
+function collap1(){
+  document.getElementById('minus1').style.display='none';
+  document.getElementById('plus1').style.display = 'inline';
+  document.getElementById('hid1').style.display  = 'none';
+}
+function constr1(){
+  document.getElementById('minus1').style.display='inline';
+  document.getElementById('plus1').style.display = 'none';
+  document.getElementById('hid1').style.display  = 'block';
+}
+function collap2(){
+  document.getElementById('minus2').style.display='none';
+  document.getElementById('plus2').style.display = 'inline';
+  document.getElementById('hid2').style.display  = 'none';
+}
+function constr2(){
+  document.getElementById('minus2').style.display='inline';
+  document.getElementById('plus2').style.display = 'none';
+  document.getElementById('hid2').style.display  = 'block';
+}
+function collap3(){
+  document.getElementById('minus3').style.display='none';
+  document.getElementById('plus3').style.display = 'inline';
+  document.getElementById('hid3').style.display  = 'none';
+}
+function constr3(){
+  document.getElementById('minus3').style.display='inline';
+  document.getElementById('plus3').style.display = 'none';
+  document.getElementById('hid3').style.display  = 'block';
+}
+function collap4(){
+  document.getElementById('minus4').style.display='none';
+  document.getElementById('plus4').style.display = 'inline';
+  document.getElementById('hid4').style.display  = 'none';
+}
+function constr4(){
+  document.getElementById('minus4').style.display='inline';
+  document.getElementById('plus4').style.display = 'none';
+  document.getElementById('hid4').style.display  = 'block';
+}
